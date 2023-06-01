e057a60b8a211ccf03db3d3f7eeab296944250ba
pastafartian <728294+pastafartian@users.noreply.github.com>
2023-06-01 12:52:40 -0400

    simplified Display

--- src/Display.jsx
+++ src/Display.jsx
@@ -2,15 +2,9 @@
 
 
 export default function Display({ value, input}) {
-    if (input != '') {
-        return (
-            <div id="display">
-                <p>{input}</p>
-            </div>
-        )
-    } else {
-        return (
-            <p id="display">{value}</p>
-        )
-    }
+    return (
+        <div id="display">
+            <h1>{input === '' ? value : input}</h1>
+        </div>
+    )
 }
